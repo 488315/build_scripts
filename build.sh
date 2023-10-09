f7eec166d75f1884e9b9826de73f9bf40cab2bb7
kjones <19639817+488315@users.noreply.github.com>
2023-10-09 00:05:10 -0700

    Update build.sh
    In this commit, we have streamlined the build process in the Bash script by replacing the separate 'make installclean' and 'make bacon' commands with a single 'cmka bacon' command. This change enhances the efficiency and readability of the script.
    
    Here's a breakdown of the improvements made:
    
    1. Unsetting _JAVA_OPTIONS:
       - We retain the ability to unset the '_JAVA_OPTIONS' environment variable if it is set. This ensures that the script does not inherit any unintended Java options.
    
    2. Setting Android Jack VM Args:
       - The 'ANDROID_JACK_VM_ARGS' variable is defined with the specified JVM arguments, which are commonly used for Android development.
    
    3. Simplified Kernel Build Cleanup:
       - The script now removes unnecessary files in the kernel build directory to prevent counting up numbers. This cleanup is performed in a more concise manner.
    
    4. Improved Clarity:
       - We have added comments for clarity throughout the script, making it easier to understand the purpose of each section.
    
    5. 'cmka bacon':
       - The most significant change is the replacement of 'make installclean' and 'make bacon' with a single 'cmka bacon' command. This utilizes 'cmka' to build the project with the 'bacon' target, simplifying the build process and enhancing script readability.
    
    Overall, this commit optimizes the script for building the LineageOS project, ensuring a cleaner, more efficient, and maintainable script for developers and users alike.

--- build.sh
+++ build.sh
@@ -2,20 +2,24 @@
 clear
 var=userdebug
 device=karnak
-# unexport _JAVA_OPTIONS of availible in .bashrc
-if  $_JAVA_OPTIONS
-then
-export -n _JAVA_OPTIONS
-fi
-export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
-echo "[BUILD] Building lineage_$device-$var"
-echo "[ENVIROMENT] Setting Enviroment for $device"
-source build/envsetup.sh
-# prevent from the kernel build counting up the numbers
-rm -rf out/target/product/$device/obj/KERNEL_OBJ/.version
-rm -rf out/target/product/$device/obj/KERNEL_OBJ/.config.old
-echo "[BUILD] Adding $device to the lunch menu"
-lunch lineage_$device-$var
-#make installclean and then make bacon. make clean is implemented because to fix vendor image mismatch
-cmka bacon
 
+# Unset _JAVA_OPTIONS if it's set
+if [[ ! -z "${_JAVA_OPTIONS+x}" ]]; then
+  unset _JAVA_OPTIONS
+fi
+
+ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
+
+echo "[BUILD] Building lineage_$device-$var"
+echo "[ENVIRONMENT] Setting Environment for $device"
+source build/envsetup.sh
+
+# Prevent kernel build from counting up the numbers
+rm -rf "out/target/product/$device/obj/KERNEL_OBJ/.version"
+rm -rf "out/target/product/$device/obj/KERNEL_OBJ/.config.old"
+
+echo "[BUILD] Adding $device to the lunch menu"
+lunch "lineage_$device-$var"
+
+# Use cmka to build with 'bacon' target
+cmka bacon
